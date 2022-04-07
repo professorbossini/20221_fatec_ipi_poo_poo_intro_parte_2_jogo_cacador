dd9eff955acf3673a5cecf619cf5c7dd99561956
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-07 18:25:38 -0300

    Encerramos a apostila 06

--- Jogo.java
+++ Jogo.java
@@ -1,14 +1,45 @@
+import java.util.Random;
 public class Jogo {
-    public static void main(String[] args) {
-        Personagem p1 = new Personagem();
-        Personagem p2 = new Personagem();
+    public static void main(String[] args) throws Exception{
+        Personagem cacador = new Personagem(10, 0, 0);
+        Personagem soneca = new Personagem (2, 4, 10);
+        Personagem guloso = new Personagem(4, 10, 2);
+        cacador.setNome("Caçador");
+        Random gerador = new Random();
+        // 1: cacar
+        // 2: comer
+        // 3: dormir    
+
+        while(true){
+            int oQueFazer = + gerador.nextInt(3) + 1; //[1, 3]
+            System.out.println(oQueFazer);
+            //fall-through
+            switch(oQueFazer){
+                case 1:
+                    cacador.cacar();
+                    break;
+                case 2:
+                    cacador.comer();
+                    break;
+                case 3:
+                    cacador.dormir();
+                    break;
+            }
+            System.out.println("======================================");
+            Thread.sleep(2000);
+        }
         
-        System.out.printf("%d %d", p1.energia, p2.energia);
-        // Personagem p = new Personagem();
-        // p.nome = "João";
-        // p.cacar();
-        // p.dormir();
-        // p.comer(); 
+        
+        
+        // // Personagem p1 = new Personagem();
+        // // Personagem p2 = new Personagem();
+        
+        // // System.out.printf("%d %d", p1.energia, p2.energia);
+        // // // Personagem p = new Personagem();
+        // // // p.nome = "João";
+        // // // p.cacar();
+        // // // p.dormir();
+        // // // p.comer(); 
         
         
     }
--- Personagem.java
+++ Personagem.java
@@ -1,10 +1,35 @@
 public class Personagem{
-    String nome;
-    int energia;
-    int fome;
-    int sono;
-    final static int LIMITE_SUPERIOR = 10;
+    //byte, short, int, long, float, double: 0
+    //char: \0
+    //boolean: false
+    //variáveis de referência (String, Personagem): null
+    private String nome;
+    private int energia = 10;
+    private int fome = 0;
+    private int sono = 0;
+    private final static int LIMITE_SUPERIOR = 10;
+
+    public Personagem (int energia, int fome, int sono){
+        if (energia >= 0 && energia <= 10)
+            this.energia = energia;
+        if (fome >= 0 && fome <= 10)
+            this.fome = fome;
+        if (sono >= 0 && sono <= 10)
+            this.sono = sono;
+    }
+
+    public Personagem (String nome, int energia, int fome, int sono){
+        this(energia, fome, sono);
+        this.nome = nome;
+    }
     
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
 
     void cacar(){
         if (this.energia >= 2){
@@ -14,8 +39,8 @@ public class Personagem{
         else{
             System.out.println(nome + " sem energia para caçar...");
         }
-        this.fome = Math.min (this.fome + 1, 10);
-        this.sono = Math.min(this.sono + 1, 10);
+        this.fome = Math.min (this.fome + 1, LIMITE_SUPERIOR);
+        this.sono = Math.min(this.sono + 1, LIMITE_SUPERIOR);
     }
 
     void comer(){
@@ -23,17 +48,24 @@ public class Personagem{
             System.out.println(this.nome + " comendo...");
             this.fome -= 1;
             //this.energia += 1;
-            this.energia = this.energia + 1 <= 10 ? this.energia + 1 : this.energia;
+            this.energia = this.energia + 1 <= LIMITE_SUPERIOR ? this.energia + 1 : this.energia;
         }
         else{
-            System.out.printf("%s sem fome...\n", nome);
+            System.out.printf("%s sem fome...\n", this.nome);
         }
     }
 
     void dormir(){
-        System.out.println(String.format(
-            "%s dormindo...",
-            this.nome
-        ));
+        if (this.sono >= 1){
+            System.out.println(String.format(
+                "%s dormindo...",
+                this.nome
+            ));
+            this.sono -= 1;
+            this.energia = this.energia + 1 <= LIMITE_SUPERIOR ? this.energia + 1 : this.energia;
+        }
+        else{
+            System.out.println (nome + " sem sono...");
+        }
     }
 }
