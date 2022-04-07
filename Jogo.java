c822d9630d4736812b16927a5fe0587fd442ae87
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-07 16:39:11 -0300

    Inicio do jogo'

---
+++ Jogo.java
@@ -0,0 +1,15 @@
+public class Jogo {
+    public static void main(String[] args) {
+        Personagem p1 = new Personagem();
+        Personagem p2 = new Personagem();
+        
+        System.out.printf("%d %d", p1.energia, p2.energia);
+        // Personagem p = new Personagem();
+        // p.nome = "João";
+        // p.cacar();
+        // p.dormir();
+        // p.comer(); 
+        
+        
+    }
+}
--- Personagem.java
+++ Personagem.java
@@ -3,19 +3,37 @@ public class Personagem{
     int energia;
     int fome;
     int sono;
+    final static int LIMITE_SUPERIOR = 10;
+    
 
     void cacar(){
-        System.out.println(nome + " caçando...");
+        if (this.energia >= 2){
+            System.out.println(nome + " caçando...");
+            this.energia = this.energia - 2;
+        }
+        else{
+            System.out.println(nome + " sem energia para caçar...");
+        }
+        this.fome = Math.min (this.fome + 1, 10);
+        this.sono = Math.min(this.sono + 1, 10);
     }
 
     void comer(){
-        System.out.printf("%s comendo...\n", nome);
+        if (this.fome >= 1){
+            System.out.println(this.nome + " comendo...");
+            this.fome -= 1;
+            //this.energia += 1;
+            this.energia = this.energia + 1 <= 10 ? this.energia + 1 : this.energia;
+        }
+        else{
+            System.out.printf("%s sem fome...\n", nome);
+        }
     }
 
     void dormir(){
         System.out.println(String.format(
             "%s dormindo...",
-            nome
+            this.nome
         ));
     }
 }
